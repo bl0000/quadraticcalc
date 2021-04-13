830c0c416a01e3e0904b13f281cd1f1e96b6ac98
bl0000 <82478568+bl0000@users.noreply.github.com>
2021-04-13 23:57:17 +0100

    Added main.py
    The quadratic calculator script.

---
+++ main.py
@@ -0,0 +1,24 @@
+# Quadratic calculator using the formula
+# ax^2 + bx + c
+
+import math
+
+def quadratic():
+    print("ax^2 + bx + c")
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+    print(a, b, c)
+    minusb = -b
+    sr = math.sqrt((b * b)-(4 * a * c))
+    total = (minusb + sr) / (2 * a)
+    total2 = (minusb - sr) / (2 * a)
+    print("x =", total, "or x =", total2)
+
+def main():
+    quadratic()
+
+if __name__ == '__main__':
+    while True:
+        main()
+
